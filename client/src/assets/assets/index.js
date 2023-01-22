810c03440599771235d79ea3bc73eba705556563
Aneesh <sunnykumar12928@gmail.com>
2023-01-22 12:21:23 +0530

    fronted created

---
+++ client/src/assets/assets/index.js
@@ -0,0 +1,11 @@
+import download from './download.png';
+import logo from './logo.svg';
+import preview from './preview.png';
+import neurograph from './neurograph.png'
+
+export {
+  download,
+  logo,
+  preview,
+  neurograph
+};
